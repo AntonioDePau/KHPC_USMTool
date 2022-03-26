41615a7d22100562e4649d212c818fd7b3e64862
AntonioDePau <antoine301191@hotmail.fr>
2022-03-26 19:43:06 +0000

    v1.0.1

--- AssemblyInfo.cs
+++ AssemblyInfo.cs
@@ -1,5 +1,5 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+[assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.1.0")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KHPC USM Tool")]
